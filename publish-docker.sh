47df855dabdce6c2411ba6d1e39bc4e66e9743a7
Liam Sho <conan_creeper@outlook.com>
2022-03-11 15:36:19 +0800

    fix.修复构建脚本错误

--- publish-docker.sh
+++ publish-docker.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-docker buildx build --platform linux/amd64 -t alisaqaq/maa-download-server-runtime-environment:$1-amd64 .
-docker buildx build --platform linux/arm64 -t alisaqaq/maa-download-server-runtime-environment:$1-arm64 .
-docker buildx build --platform linux/arm/v7 -t alisaqaq/maa-download-server-runtime-environment:$1-arm-v7 .
-
-docker image push --all-tags alisaqaq/maa-download-server-runtime-environment
+docker buildx build --push --platform linux/amd64 -t alisaqaq/maa-download-server-runtime-environment:$1-amd64 .
+docker buildx build --push --platform linux/arm64 -t alisaqaq/maa-download-server-runtime-environment:$1-arm64 .
+docker buildx build --push --platform linux/arm/v7 -t alisaqaq/maa-download-server-runtime-environment:$1-arm-v7 .
 
 docker manifest create alisaqaq/maa-download-server-runtime-environment:$1 \
     alisaqaq/maa-download-server-runtime-environment:$1-amd64 \
@@ -13,7 +11,7 @@ docker manifest create alisaqaq/maa-download-server-runtime-environment:$1 \
 
 docker manifest push alisaqaq/maa-download-server-runtime-environment:$1
 
-docker manifest create alisaqaq/maa-download-server-runtime-environment:latest \
+docker manifest create --amend alisaqaq/maa-download-server-runtime-environment:latest \
     alisaqaq/maa-download-server-runtime-environment:$1-amd64 \
     alisaqaq/maa-download-server-runtime-environment:$1-arm64 \
     alisaqaq/maa-download-server-runtime-environment:$1-arm-v7
